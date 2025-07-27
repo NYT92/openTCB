5bf44dec1e822eed3475a191d5bc2031e59bf0f0
NYT92 <chamroeunlinda12343@gmail.com>
2025-07-27 23:03:53 +0700

    Add unsupported browser notice to app.vue, update localization strings for English and Khmer, and remove unused localization files.

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
+  <div
+    v-if="isUnsupportedBrowser"
+    class="min-h-screen flex items-center justify-center flex-col gap-4"
+  >
+    <p class="text-center text-xl">
+      {{ t("notice") }}
+    </p>
+    <UButton
+      @click="isUnsupportedBrowser = false"
+      size="xl"
+      icon="i-heroicons-x-mark"
+    >
+      {{ t("close") }}
+    </UButton>
+  </div>
+  <div v-else>
     <header class="p-4 bg-gray-800 text-white">
       <div class="container mx-auto flex justify-between items-center">
         <h1 class="text-xl font-bold">openTCB | អូភីនធីស៊ីបឺ</h1>
         <div class="flex gap-4 items-center">
           <div class="flex gap-2 items-center">
-            <UButton @click="decreaseFontSize" color="info" variant="outline" size="xs">A-</UButton>
-            <UButton @click="increaseFontSize" color="info" variant="outline" size="xs">A+</UButton>
+            <UButton
+              @click="decreaseFontSize"
+              color="info"
+              variant="outline"
+              size="xs"
+              >A-</UButton
+            >
+            <UButton
+              @click="increaseFontSize"
+              color="info"
+              variant="outline"
+              size="xs"
+              >A+</UButton
+            >
           </div>
-          <UButton @click="toggleHighContrast" color="info" variant="outline" size="xs">
-            {{ isHighContrast ? 'Standard Contrast' : 'High Contrast' }}
+          <UButton
+            @click="toggleHighContrast"
+            color="info"
+            variant="outline"
+            size="xs"
+          >
+            {{ isHighContrast ? "Standard Contrast" : "High Contrast" }}
           </UButton>
         </div>
       </div>
@@ -20,16 +52,43 @@
       <p>
         <a href="https://github.com/NYT92/openTCB" target="_blank"> openTCB </a>
         |
-        <a href="https://github.com/NYT92" target="_blank"> Created by NYT92 🇰🇭 </a>
+        <a href="https://github.com/NYT92" target="_blank">
+          Created by NYT92 🇰🇭
+        </a>
       </p>
     </footer>
   </div>
 </template>
 
 <script setup>
-import { useAccessibility } from '~/composables/useAccessibility';
+import { useAccessibility } from "~/composables/useAccessibility";
 
-const { increaseFontSize, decreaseFontSize, isHighContrast, toggleHighContrast } = useAccessibility();
+const { t } = useI18n();
+
+const {
+  increaseFontSize,
+  decreaseFontSize,
+  isHighContrast,
+  toggleHighContrast,
+} = useAccessibility();
+
+const isUnsupportedBrowser = ref(false);
+
+onMounted(() => {
+  const userAgent = navigator.userAgent.toLowerCase();
+  // Common User-Agent keywords for in-app browsers
+  const unsupportedKeywords = [
+    "fban", // Facebook App for Android
+    "fbav", // Facebook App Version
+    "instagram", // Instagram
+    "tiktok", // TikTok
+    "musical_ly", // TikTok's old name
+  ];
+
+  isUnsupportedBrowser.value = unsupportedKeywords.some((keyword) =>
+    userAgent.includes(keyword)
+  );
+});
 </script>
 
 <style>
